f38c4373b38c33971eead6de0e61025f505566bf
sam <sgsparks@gmail.com>
2024-01-13 16:27:32 -0800

    Created Repository

---
+++ dynamicStrengh-API/src/main/java/com/DynamicStrength/dynamicStrenghAPI/DynamicStrenghApiApplication.java
@@ -0,0 +1,13 @@
+package com.DynamicStrength.dynamicStrenghAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DynamicStrenghApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DynamicStrenghApiApplication.class, args);
+	}
+
+}
---
+++ dynamicStrengh-API/src/test/java/com/DynamicStrength/dynamicStrenghAPI/DynamicStrenghApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.DynamicStrength.dynamicStrenghAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DynamicStrenghApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
