b6c6b7f79b5a5a563007893a9b9af41eb8f2eda6
sam <sgsparks@gmail.com>
2024-01-13 21:25:55 -0800

    Re built repo

---
+++ SpringAPI/src/test/java/com/DynamicStrenghBackend/SpringAPI/SpringApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.DynamicStrenghBackend.SpringAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
